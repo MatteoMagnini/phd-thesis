93884d16c91581c1d04dfa1be73be9053a0aacb0
Matteo Magnini <matteo.magnini@unibo.it>
2025-09-15 10:03:02 +0200

    fix: venn logic diagram

--- scripts/venn_diagram_logics.py
+++ scripts/venn_diagram_logics.py
@@ -10,34 +10,34 @@ ax.add_patch(fol)
 ax.text(0, 1.8, "First-Order Logic", fontsize=24, ha='center', weight='bold')
 
 # Horn Logic inside FOL, left
-horn = plt.Circle((-1.4, 0), 1.2, edgecolor='blue', facecolor='#85c1e9', lw=2)
+horn = plt.Circle((-1, -1), 1.4, edgecolor='blue', facecolor='#85c1e9', lw=2)
 ax.add_patch(horn)
-ax.text(-1.4, -0.6, "Horn Logic", fontsize=18, ha='center', color='white', weight='bold')
+ax.text(-1, 0, "Horn Logic", fontsize=18, ha='center', color='white', weight='bold')
 
 # Datalog inside Horn Logic
-datalog = plt.Circle((-1.4, 0.4), 0.5, edgecolor='navy', facecolor='#2874a6', lw=2)
+datalog = plt.Circle((-1, -1.2), 1, edgecolor='navy', facecolor='#2874a6', lw=2)
 ax.add_patch(datalog)
-ax.text(-1.4, 0.4, "Datalog", fontsize=16, ha='center', color='white', weight='bold')
+ax.text(-1, -0.6, "Datalog", fontsize=16, ha='center', color='white', weight='bold')
 
 # Description Logic inside FOL, right
-dl = plt.Circle((1.4, 0), 1.2, edgecolor='green', facecolor='#a9dfbf', lw=2)
+dl = plt.Circle((1.4, 0.2), 1.2, edgecolor='green', facecolor='#a9dfbf', lw=2)
 ax.add_patch(dl)
-ax.text(1.4, 0.45, "Description\nLogic", fontsize=18, ha='center', weight='bold')
+ax.text(1.4, 0.65, "Description\nLogic", fontsize=18, ha='center', weight='bold')
 
 # ALC inside DL
-alc = plt.Circle((1.0, -0.05), 0.4, edgecolor='darkgreen', facecolor='#58d68d', lw=2)
+alc = plt.Circle((1.0, 0.15), 0.4, edgecolor='darkgreen', facecolor='#58d68d', lw=2)
 ax.add_patch(alc)
-ax.text(1.0, -0.05, r'$\mathcal{ALC}$', fontsize=16, ha='center', weight='bold')
+ax.text(1.0, 0.15, r'$\mathcal{ALC}$', fontsize=16, ha='center', weight='bold')
 
 # EL inside DL
-el = plt.Circle((1.8, -0.55), 0.4, edgecolor='darkgreen', facecolor='#58d68d', lw=2)
+el = plt.Circle((1.8, -0.35), 0.4, edgecolor='darkgreen', facecolor='#58d68d', lw=2)
 ax.add_patch(el)
-ax.text(1.8, -0.55, r'$\mathcal{EL}$', fontsize=16, ha='center', weight='bold')
+ax.text(1.8, -0.35, r'$\mathcal{EL}$', fontsize=16, ha='center', weight='bold')
 
 # Propositional Logic inside FOL, bottom center
-pl = plt.Circle((0, -1.7), 0.75, edgecolor='orange', facecolor='#f7dc6f', lw=2)
+pl = plt.Circle((-1, -1.4), 0.65, edgecolor='orange', facecolor='#f7dc6f', lw=2)
 ax.add_patch(pl)
-ax.text(0, -1.9, "Propositional\nLogic", fontsize=16, ha='center', weight='bold')
+ax.text(-1, -1.6, "Propositional\nLogic", fontsize=14, ha='center', weight='bold')
 
 # Plot settings
 ax.set_xlim(-4, 4)
